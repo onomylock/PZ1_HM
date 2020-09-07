89508814a5f069173c978a1ec97f41739c80ba18
Никита Михайлов <onomylock@outlook.com>
2020-09-07 14:57:41 +0700

    добавить разложение векторов al & di и разработать алгоритм

--- PZ1_HM/decomposition.cpp
+++ PZ1_HM/decomposition.cpp
@@ -1,8 +1,8 @@
 #include "Header.h"
 
-void decomposition(vector<float> A, vector<int> ai, int n, int m) //Разложение матрицы А
+void decomposition(vector<float> A,  vector<int> ai, int n, int m) //Разложение матрицы А
 {
-	vector<float> L;
+	vector<float> arr, L;
 	int sign = 1;
 	float sum;
 	/*for (int i = 0; i < n; i++)
@@ -11,13 +11,19 @@ void decomposition(vector<float> A, vector<int> ai, int n, int m) //Разложение м
 		L[i] = sqrt(sign*());
 	}*/
 
-	L[0] = sqrt(A[0]);
+
+
+	L[0] = sqrt(di[0]);
 
 	for (int i = 1; i < n; i++)
 	{
+		i % 2 == 0 ? sum -= di[i] : sum += di[i];
+
 		for (int k = 0; k < i; k++) 
 		{
-			k % 2 == 0 ? sum -=  
+			k % 2 == 0 ? sum -= L[k] : sum += L[k];
 		}
+
+		L[i] = sqrt(sum);
 	}
 }
