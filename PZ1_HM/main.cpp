5efc28ff434e88de75de16c3df3d09d5ca9e4ccb
Никита Михайлов <onomylock@outlook.com>
2020-09-26 13:00:27 +0700

    финальная версия 1.1

--- PZ1_HM/main.cpp
+++ PZ1_HM/main.cpp
@@ -6,6 +6,8 @@ int main() {
 	vector<int> ai;
 	int n = 0, m = 0, ch;
 
+	input(A, F, ai, n, m);				// Считываем значения из текстовых файлов
+
 	cout << "Choose the method: " << endl;
 	cout << "1) Cholesky method: " << endl;
 	cout << "2) Gaussian method: " << endl;
@@ -14,30 +16,39 @@ int main() {
 
 	switch (ch)
 	{
-	case 1:
-	{
-		input(A, F, ai, n, m);				// Считываем значения из текстовых файлов
-		decomposition(A, ai, n, m);		// Раскладываем матрицу A и находим значения матрицы L
-		straight_step(A, ai, F, n, m);	// Прямой ход решения СЛАУ
-		middle_step(F, n);					// Средний ход решения СЛАУ
-		reverse_step(A, F, ai, n, m);		// Обратный ход решения СЛАУ, запись результата в массив F
-		break;
-	}
-	case 2:
-	{
-		break;
-	}
-	}
+		case 1:
+		{
+			decomposition(A, ai, n, m);		// Раскладываем матрицу A и находим значения матрицы L
+			straight_step(A, ai, F, n, m);	// Прямой ход решения СЛАУ
+			middle_step(F, n);					// Средний ход решения СЛАУ
+			reverse_step(A, F, ai, n, m);		// Обратный ход решения СЛАУ, запись результата в массив F
+		
+			ofstream out("output_X.txt");
 
+			for (int i = 0; i < n; i++)
+			{
+				out << F[i] << endl;
+			}
+			out.close();
+		
+			break;
+		}
+
+		case 2:
+		{
+			gauss(A, F, ai, n, m);
+
+			ofstream out("output_G.txt");
+
+			for (int i = 0; i < n; i++)
+			{
+				out << F[i] << endl;
+			}
+			out.close();
+
+			break;
+		}
+	}
 	
-	
-	ofstream out("output.txt");
-
-	for (int i = 0; i < n; i++)
-	{
-		out << F[i] << endl;
-	}
-	out.close();
-
 	return 0;
 }
